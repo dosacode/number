1981c7b4f1f833d1fc75eb716149b1d5b10d9508
dosacode <d133359@gmail.com>
2019-04-29 00:46:18 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  /*$plus1 = 5;
+  $plus2 = 2;
+
+  $plus3 = $plus1 + $plus2;
+
+?>*/
+
+$number1 = rand(0,10);
+
+$number2 = rand(0,10);
+
+
+$number3 = $number1 + $number2;
+
+
+
+
+if ($number3 == $choicenumber) {
+  echo "せいかいだよ！";
+}else{
+  echo "まちがっているよ";
+}
+  // code...
+  ArrayAccess
+
+?>
