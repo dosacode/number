23d93eec58e9f079381a23dc5131a36b50be96af
dosacode <d133359@gmail.com>
2019-10-13 16:49:42 +0900

    first commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require ("./number.php");
+require "number.php";
 
 
 ?>
@@ -7,21 +7,21 @@ require ("./number.php");
 <html lang = "ja">
 <head>
     <meta charset = "UFT-8">
-    <link rel="stylesheet" href="/Users/saico/Desktop/number/plus.css">
-    <link rel="stylesheet" href="/Users/saico/Desktop/number/index.php">
+    <link rel="stylesheet" href="/Library/Webserver/Dcuments/plus.css">
+    <!--<link rel="stylesheet" href="/Users/saico/Desktop/number/index.php">-->
 
   <title>けいさん</title>
 </head>
 <body>
   <h1>Apple+</h1>
-      <form action = index13.php"　method" = "get">
+      <form action = ""　method = "get">
 <!--<input type = “submit” name =“comment/“>-->
-  <div class="circle">
+  <div class= "circle">
     <span>？</span>
     <?php echo $number1; ?>
   </div>
   <p>けいさんしてみよう。</p>
-      <button type ="submit" value ="送信">
+      <button type = "submit" value = "送信">
       </form>
 </body>
 </html>
